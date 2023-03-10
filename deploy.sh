f46edcc71a363ee672734f86fd7cbea4dcd00ba0
wuxs <317009160@qq.com>
2023-03-11 01:29:49 +0800

    fix:部署方式分支

--- deploy.sh
+++ deploy.sh
@@ -15,7 +15,7 @@ git init
 git add -A
 git commit -m 'add'
 
-git push -f git@github.com:imycod/blog_imycode.git main:blog
+git push -f git@github.com:imycod/blog_imycode.git master:main
 # git push -f git@github.com:你的git名/你的git项目名.git master:你的git分支
 
 cd -
